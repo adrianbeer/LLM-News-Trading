eb57116a657eec1429c5a33e6d27b8cbf9888a53
adrianbeer <adrianbeer@fastmail.com>
2023-06-22 13:49:56 +0200

    Improves text preprocessing

--- src/preprocessing/news_importer.py
+++ src/preprocessing/news_importer.py
@@ -9,26 +9,75 @@ api_key = "63bcced6972b4b88b66b0e669336555f"
 fin = financial_data.Benzinga(api_key)
 paper = news_data.News(api_key)
 
-all_stories = []
+with open("data/tickers.pkl",'rb') as f:
+    TICKERS = pickle.load(f)
 
-RELEVANT_CHANNELS = ["Earnings", "Dividends", "News", "Financing", "Events"]
+# "News"
+RELEVANT_CHANNELS = ["Earnings", "Dividends", "Financing", "Events"]
 
 # source = open("data/tickers.pkl", 'rb').read()
 # tickers = json.loads(source)
 
+
+def parse_story_to_row(story):
+    # Converts JSON (dict) story to a list 
+    tags = [d["name"] for d in story["tags"]]
+    channels = [d["name"] for d in story["channels"]]
+
+    if len(set(channels).intersection(set(RELEVANT_CHANNELS))) == 0: 
+        return None
+    
+    stocks = [d["name"] for d in story["stocks"]]
+    if len(stocks) != 1: 
+        return None
+    
+    stocks = stocks[0] # This is the ticker(s)
+    if stocks not in TICKERS: return None # Don't process, if we have no stock data for it
+
+    body = story["body"]
+    time = pd.to_datetime(story["created"])
+    title = story["title"]
+    author = story["author"]
+    id = story["id"]
+    return id, title, tags, json.dumps(channels), stocks, body, body_formatter(body), time, author
+
+
+def body_formatter(body):
+    soup = BeautifulSoup(body, features="html.parser")
+    for t in soup.find_all('table'):
+        t.decompose()
+
+    new_body = str(soup)
+    h = html2text.HTML2Text()
+    h.ignore_links = True
+    h.ignore_images = True
+    # h.bypass_tables = True
+    # h.ignore_emphasis = True
+    h.drop_white_space = True
+    return h.handle(new_body)
+
+
 if __name__ == "__main__":
+    story_df = pd.DataFrame(columns=["time", "stocks", "author", "title", "channels", "body", "html_body"], dtype=object)
+    
     no_error = True
     i = 0
-    while no_error and i <= 2:
+    while no_error and i <= 5:
         try:
             #  channel="Earnings"
             stories = paper.news(display_output="full", page=i, channel=",".join(RELEVANT_CHANNELS), pagesize=1000)
-            all_stories.append(stories)
+            for s in range(len(stories)):
+                story = stories[s]
+                parsed_story = parse_story_to_row(story)
+                if parsed_story is None: continue
+                id, title, tags, channels, stocks, html_body, body, time, author = parsed_story
+                #if "Earnings" in channels:
+                story_df.loc[id, ["time", "stocks", "author", "title", "channels", "body", "html_body"]] = time, stocks, author, title, channels, body, html_body
             i += 1
         except Exception as e:
             print(e) 
             no_error = False
+    print(f"#rows: {story_df.shape[0]}")
+    with open("data/story_df_raw.pkl", "wb") as f:
+        pickle.dump(story_df, f)
 
-    print(f"Pagesize: {len(all_stories[0])}")
-    with open("data/stories_raw.pkl", "wb") as f:
-        pickle.dump(all_stories, f)
--- src/preprocessing/news_parser.py
+++ src/preprocessing/news_parser.py
@@ -17,44 +17,6 @@ warnings.filterwarnings("ignore", category=UnknownTimezoneWarning)
 with open("data/tickers.pkl",'rb') as f:
     TICKERS = pickle.load(f)
 
-def parse_story_to_row(story):
-    # Converts JSON (dict) story to a list 
-    tags = [d["name"] for d in story["tags"]]
-    channels = [d["name"] for d in story["channels"]]
-
-    if len(set(channels).intersection(set(RELEVANT_CHANNELS))) == 0: 
-        return None
-    
-    stocks = [d["name"] for d in story["stocks"]]
-    if len(stocks) != 1: 
-        return None
-    
-    stocks = stocks[0] # This is the ticker(s)
-    if stocks not in TICKERS: return None # Don't process, if we have no stock data for it
-
-    body = story["body"]
-    time = pd.to_datetime(story["created"])
-    title = story["title"]
-    author = story["author"]
-    id = story["id"]
-    return id, title, tags, channels, stocks, body, time, author
-
-
-def body_formatter(body):
-    soup = BeautifulSoup(body, features="html.parser")
-    for t in soup.find_all('table'):
-        t.decompose()
-
-    new_body = str(soup)
-    h = html2text.HTML2Text()
-    h.ignore_links = True
-    h.ignore_images = True
-    # h.bypass_tables = True
-    # h.ignore_emphasis = True
-    h.drop_white_space = True
-    
-    return h.handle(new_body)
-
 
 def filter_body(body, ticker, author, pr_date):
     # Remove links
@@ -87,11 +49,11 @@ def filter_body(body, ticker, author, pr_date):
         pass
     
     # Remove ticker info
-    body = re.sub(f"\([A-Z]*:{ticker}\)", "REMOVE_THIS", body)
+    body = re.sub(f"\([A-Z]*[ ]?:[ ]?{ticker}\)", "REMOVE_THIS", body)
     body = body.replace("REMOVE_THIS", "")
 
     # Remove Dates
-    dates = datefinder.find_dates(body, source=True)
+    dates = datefinder.find_dates(body, source=True, strict=True)
     for date, date_string in dates:
         if pr_date.date() < date.date():
             body = body.replace(date_string, "a past date")
@@ -102,32 +64,23 @@ def filter_body(body, ticker, author, pr_date):
     #body = re.sub(" [A-Z][a-z]* [0-9][0-9], [0-9]* ", " ", body)
     
     # Remove author (preamble)
-    body = re.sub(f".*{author}", "", body, flags=re.IGNORECASE) # TODO: ZUSAMMENFASSUNG KOMMT EVEL. VOR AUTHOR PRÄEMBEL, DANN IST es schlecht, alles vorher zu löschen
-    body = re.sub("^((\)|-| |\/|\\\\|_)*)", "", body)
+    body = re.sub(f"(\n.*{author})|(^.*{author})", "\n", body, flags=re.IGNORECASE) # TODO: ZUSAMMENFASSUNG KOMMT EVEL. VOR AUTHOR PRÄEMBEL, DANN IST es schlecht, alles vorher zu löschen
+    
+    # Remove weird symbols
+    SYMBOLS_REGEX = "(\)|-| |\/|\\\\|_)"
+    body = re.sub(f"\n{SYMBOLS_REGEX}*|^({SYMBOLS_REGEX}|\")", "\n", body)
+    body = body.replace("*", "")
+
+    # Remove (the "Company") parenthesis 
+    body = re.sub('(.*"Company".*)', "", body)
+    body = body.strip("\n - \\")
+
     return body
 
 
 if __name__ == "__main__":
-    with open("data/stories_raw.pkl", 'rb') as f:
-        all_stories = pickle.load(f)
-
-    stories = list(itertools.chain(*all_stories))
-    print(f"Total stories: {len(stories)}")
-
-    story_df = pd.DataFrame(columns=["time", "stocks", "author", "title", "channels", "body", "html_body"], dtype=object)
-    for i in range(len(stories)):
-        story = stories[i]
-        parsed_story = parse_story_to_row(story)
-        if parsed_story is None: continue
-        id, title, tags, channels, stocks, body, time, author = parsed_story
-        #if "Earnings" in channels:
-        story_df.loc[id, "time"] = time
-        story_df.loc[id, "stocks"] = stocks
-        story_df.loc[id, "author"] = author
-        story_df.loc[id, "title"] = title
-        story_df.loc[id, "channels"] = json.dumps(channels)
-        story_df.loc[id, "body"] = body_formatter(body)
-        story_df.loc[id, "html_body"] = body
+    with open("data/story_df_raw.pkl", 'rb') as f:
+        story_df = pickle.load(f)
 
     print(f"Filtered stories: {story_df.shape[0]}")
 
