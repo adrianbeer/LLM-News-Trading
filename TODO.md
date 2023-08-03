06d31d6e0c8b1142d000a768d88f168a50efedf9
adrianbeer <adrianbeer@fastmail.com>
2023-08-03 22:41:21 +0200

    Adjust for Benzinga idiosynchracies before 2017 when importing news

--- TODO.md
+++ TODO.md
@@ -5,6 +5,8 @@
 # Then neural_net.py, where the neural network is trained
 # Then neural_net_evaluation where the performance of the neurlal network that was trained in the last step is eavluated
 
+4. Authorenfeld für Daten von vor 2017 berichtigen und evtl. noch andere Felder? (Aktienbezug, Datum?)
+
 5. can NN relate dividend and share price? JA -> Fundamentaldaten als Text einfügen.
 
 7. Berücksichtigen wann die Nachrichten rauskommen und lieber C-C anstatt O-C betrachten, um, Gaps mitzuerfassen.
@@ -14,3 +16,10 @@
 9. Pharmaceuticals ausschliessen (Suche nach Phase 2b oder soetwas)
 
 10. NN-Vorhersagen nutzen und Clustern, um besten Trades zu identifizieren.
+
+------------------
+
+# Langfristige Ideen:
+- Sell-on-good-news
+- Buy-on-bad-news
+- GD-Richtungsfilter
--- src/preprocessing/news_importer.py
+++ src/preprocessing/news_importer.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import pickle
 import time
 
-api_key = "63bcced6972b4b88b66b0e669336555f"
+api_key = "272764ec2a3b4c3188dbef1310a699e7"
 fin = financial_data.Benzinga(api_key)
 paper = news_data.News(api_key)
 
@@ -16,16 +16,18 @@ with open("data/tickers.pkl",'rb') as f:
 # "News"
 RELEVANT_CHANNELS = ["Earnings", "Dividends", "Financing", "Events"]
 
+NO_CHANNEL_TAG_CUTOFF_DATE = pd.Timestamp(year=2016, month=12, day=31)
 
 def parse_story_to_row(story):
     # Converts JSON (dict) story to a list 
-    tags = [d["name"] for d in story["tags"]]
-    channels = [d["name"] for d in story["channels"]]
+    tags: list  = [d["name"] for d in story["tags"]]
+    channels: list = [d["name"] for d in story["channels"]]
 
-    if len(set(channels).intersection(set(RELEVANT_CHANNELS))) == 0: 
-        return None
-    
+    time = pd.to_datetime(story["created"])
     stocks = [d["name"] for d in story["stocks"]]
+
+    if time.date() > NO_CHANNEL_TAG_CUTOFF_DATE.date() and (len(set(channels).intersection(set(RELEVANT_CHANNELS))) == 0): 
+        return None
     if len(stocks) != 1: 
         return None
     
@@ -33,10 +35,10 @@ def parse_story_to_row(story):
     if ticker not in TICKERS.categories: return None # Don't process, if we have no stock data for it
 
     body = story["body"]
-    time = pd.to_datetime(story["created"])
     title = story["title"]
     author = story["author"]
     id = story["id"]
+
     return id, title, tags, json.dumps(channels), ticker, body, body_formatter(body), time, author
 
 
@@ -55,50 +57,73 @@ def body_formatter(body):
     return h.handle(new_body)
 
 
+def save_df_and_return_empty(story_df, current_year):
+    print(f"#rows: {story_df.shape[0]}")
+
+    with open(f"data/story_df_raw_{current_year}.pkl", "wb") as f:
+        pickle.dump(story_df, f)
+
+    story_df = pd.DataFrame(columns=["time", "stocks", "author", "title", "channels", "body", "html_body"], dtype=object)
+    story_df = story_df.astype({"stocks": TICKERS})   
+    return story_df
+
+
 if __name__ == "__main__":
     # Initialize empty data frame
     story_df = pd.DataFrame(columns=["time", "stocks", "author", "title", "channels", "body", "html_body"], dtype=object)
     story_df = story_df.astype({"stocks": TICKERS})
     
-    no_error = True
+    
     # `i`  together with `pagesize` specifies how many stories entries will be downloaded
     # Not all these stories will be valid news, so the size of the resulting data frame has to be checked
-    i = 1
+    
+    
+    # 100 is the max pagesize.
     pagesize = 100
+
     current_year = 0
-    while no_error and i <= 5:
-        try:
-            time.sleep(2)
-            print(i)
-            #  channel="Earnings"
-            stories = paper.news(display_output="full", page=i, channel=",".join(RELEVANT_CHANNELS), pagesize=pagesize)
-            for s in range(len(stories)):
-                story = stories[s]
-                parsed_story = parse_story_to_row(story)
-                if parsed_story is None: continue
-                id, title, tags, channels, stocks, html_body, body, timestamp, author = parsed_story
-                
-                year = timestamp.year
-                if current_year != year:
-                    print(f"#rows: {story_df.shape[0]}")
-                    with open(f"data/story_df_raw_{year}.pkl", "wb") as f:
-                        pickle.dump(story_df, f)
 
-                    story_df = pd.DataFrame(columns=["time", "stocks", "author", "title", "channels", "body", "html_body"], dtype=object)
-                    story_df = story_df.astype({"stocks": TICKERS})   
-                    current_year = year
-                    print(year)      
+    # Before 2017 there are no channels!
+    ranger_time = pd.date_range(start="2015-01-01", end="2016-12-31", freq="W") 
+    ranger = [x.strftime("%Y-%m-%d") for x in ranger_time]
 
-                #if "Earnings" in channels:
-                story_df.loc[id, ["time", "stocks", "author", "title", "channels", "body", "html_body"]] = timestamp, stocks, author, title, channels, body, html_body
+    for r in range(len(ranger) + 1):
+        i = 0
+        no_error = 0
+        while no_error <= 2:
+            try:
+                time.sleep(2)
+                print(i)
+                if ranger_time[r+1] <= NO_CHANNEL_TAG_CUTOFF_DATE:
+                    stories = paper.news(display_output="full", page=i, date_from=ranger[r], date_to=ranger[r+1], pagesize=100)
+                else:
+                    stories = paper.news(display_output="full", page=i, date_from=ranger[r], date_to=ranger[r+1], pagesize=100, channel=",".join(RELEVANT_CHANNELS))
+                print(f"Number of stories {len(stories)}")
+                if len(stories) == 0: 
+                    no_error += 1
+                for s in range(len(stories)):
+                    story = stories[s]
+                    parsed_story = parse_story_to_row(story)
 
-            i += 1
-        except Exception as e:
-            print(e) 
-            no_error = False
+                    if parsed_story is None: continue
+                    id, title, tags, channels, stocks, html_body, body, timestamp, author = parsed_story
+                    
+                    year = timestamp.year
+
+                    if current_year != year:
+                        story_df = save_df_and_return_empty(story_df, current_year)
+                        current_year = year
+                        print(year)
+
+                    story_df.loc[id, ["time", "stocks", "author", "title", "channels", "body", "html_body"]] = timestamp, stocks, author, title, channels, body, html_body
+
+                i += 1
+            except Exception as e:
+                print(e) 
+                no_error += 1
 
     print(f"#rows: {story_df.shape[0]}")
-    with open(f"data/story_df_raw_{year}.pkl", "wb") as f:
+    with open(f"data/story_df_raw_{current_year}.pkl", "wb") as f:
         pickle.dump(story_df, f)
 
     print(story_df.dtypes)
