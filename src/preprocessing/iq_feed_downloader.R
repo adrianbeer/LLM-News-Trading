b22d2358dfce85647985e0c89c4bcfd4bcffa7ab
adrianbeer <adrianbeer@fastmail.com>
2023-12-11 21:51:14 +0100

    Save tick data download code before deleting it

--- src/preprocessing/iq_feed_downloader.R
+++ src/preprocessing/iq_feed_downloader.R
@@ -1,3 +1,11 @@
+# Speicherung von IQFeed-Daten:
+# ------------------------------
+# `store_iqfeed_data` ist für das Speichern von Tick-Daten. Tickdaten sind viel zu
+# Speicherintensiv momentan, weswegen wir nur 1min-Frequenzdaten betrachten, die wir 
+# normal über get_iqfeed_data(symbol, from, to, period='1min') herunterladen.
+
+
+
 # ?QuantTools_settings
 library(QuantTools)
 QuantTools_settings_defaults()
@@ -55,19 +63,39 @@ QuantTools_settings( settings )
 store_iqfeed_data()
 
 # Get From Local Storage
-aapl <- get_iqfeed_data( symbol = 'AAPL', from = '2023-10-02 15:00:00', to = '2023-10-03 09:33:00', period = 'minute', local = T )
+aapl <- get_iqfeed_data( symbol = 'AAPL', from = '2023-10-02 15:00:00', to = '2023-10-03 09:33:00', period ='1min', local = T )
 
-aapl <- aapl[aapl$time >= as.POSIXlt("2023-10-02 15:58:00", tz="UTC")]
-aapl <- aapl[aapl$trade_market_center == 19]
-aapl <- aapl[aapl$trade_conditions == 87]
+aapl <- aapl[aapl$time >= as.POSIXlt("2023-10-03 09:30:00", tz="UTC")]
+# aapl <- aapl[aapl$trade_market_center == 19]
+# aapl <- aapl[aapl$trade_conditions == 87]
 aapl <- aapl[aapl$volume >= 5]
-aapl_price <- na.omit(aapl[,.(time,price)])
+aapl_price <- na.omit(aapl[,.(time, open, high, low, close, volume)])
 
 # Plot data
 plot_ts( aapl_price )
 
 
-
-
+############ Tick-Data and Conversion to OHLC ################
+# aapl <- get_iqfeed_data( symbol = 'AAPL', from = '2023-10-02 15:00:00', to = '2023-10-03 09:33:00', period ='tick', local = T )
+# aapl <- aapl[aapl$time >= as.POSIXlt("2023-10-03 09:00:00", tz="UTC")]
+# 
+# aapl <- aapl[aapl$trade_market_center == 19]
+# aapl <- aapl[(aapl$trade_conditions == "01")]
+# aapl <- aapl[aapl$volume >= 5]
+# aapl_price <- na.omit(aapl[,.(time, price)])
+# # Plot data
+# plot_ts( aapl_price )
+# 
+# ohlc <- function(ttime,tprice,tvolume,fmt) {
+#   ttime.int <- format(ttime,fmt)
+#   data.frame(time = ttime[tapply(1:length(ttime),ttime.int,function(x) {head(x,1)})],
+#              .Open = tapply(tprice,ttime.int,function(x) {head(x,1)}), 
+#              .High = tapply(tprice,ttime.int,max),
+#              .Low = tapply(tprice,ttime.int,min),
+#              .Close = tapply(tprice,ttime.int,function(x) {tail(x,1)}),
+#              .Volume = tapply(tvolume,ttime.int,function(x) {sum(x)}),
+#              .Adjusted = tapply(tprice,ttime.int,function(x) {tail(x,1)}))
+# } 
+# ohlc(aapl$time, aapl$price, aapl$volume, fmt="%y-%m-%d %H:%M")
 
 
