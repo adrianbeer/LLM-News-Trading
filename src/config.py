6424f5d788c2f4ceb514259c58158782328b130e
adrianbeer <adrianbeer@fastmail.com>
2024-01-09 15:24:58 +0100

    Creates daily time series from intra day prices

--- src/config.py
+++ src/config.py
@@ -8,7 +8,8 @@ config_path = "src/config.yaml"
 # Get path via environment variable iff possible 
 try:
     config_path = os.environ["TRADING_BOT_CONFIG_PATH"]
-except FileNotFoundError:
+except KeyError:
     pass
 
 config = DotMap(yaml.safe_load(open(config_path)), _dynamic=False)
+
