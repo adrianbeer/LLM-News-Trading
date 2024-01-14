bb205954dd08d36966bc06d0ecc9c21c4ce55e42
adrianbeer <adrianbeer@fastmail.com>
2024-01-14 17:45:37 +0100

    Makes base BERT model configurable in .yaml

--- src/model/neural_network.py
+++ src/model/neural_network.py
@@ -16,14 +16,26 @@ from functools import partial
 import os
 
 
-TRANSFORMER_HF_ID = 'yiyanghkust/finbert-fls'
+class BERTClassifier(nn.Module):
+    def __init__(self, bert_model_name, num_classes):
+        super(BERTClassifier, self).__init__()
+        self.bert = BertModel.from_pretrained(bert_model_name)
+        self.dropout = nn.Dropout(0.1)
+        self.fc = nn.Linear(self.bert.config.hidden_size, num_classes)
+
+def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = outputs.pooler_output
+        x = self.dropout(pooled_output)
+        logits = self.fc(x)
+        return logits
 
 
 class MyBertModel(nn.Module):
    
-   def __init__(self):
+   def __init__(self, bert_model_name):
        super(MyBertModel, self).__init__()
-       self.bert = BertModel.from_pretrained(TRANSFORMER_HF_ID)
+       self.bert = BertModel.from_pretrained(bert_model_name)
        D_in, D_out = self.bert.config.hidden_size, 1
        self.regr = nn.Sequential(
            nn.Dropout(0.2),
