fdd42fd586ff6d40624f6d98f8e8e99ffc76437f
adrianbeer <adrianbeer@fastmail.com>
2024-01-18 14:39:08 +0100

    Work on embedding calculation

--- src/model/data_loading.py
+++ src/model/data_loading.py
@@ -2,8 +2,6 @@ import pandas as pd
 import torch
 from torch import Tensor
 from torch.utils.data import DataLoader, TensorDataset
-from src.config import config, MODEL_CONFIG
-from src.model.neural_network import embed_inputs
 from typing import List
 
 
@@ -14,9 +12,9 @@ def get_data_loader_from_dataset(dataset: pd. DataFrame,
                                  data_loader_kwargs: dict = dict()):
     if split:
         dataset = dataset.loc[dataset["split"] == split, :] 
-    inputs = dataset["input_id"]
-    masks = dataset["mask"]
-    labels = dataset[label_col]
+    inputs: pd.Series = dataset["input_id"]
+    masks: pd.Series = dataset["mask"]
+    labels: pd.Series = dataset[label_col]
     dataloader: DataLoader = create_dataloaders(inputs, 
                                                 masks, 
                                                 labels, 
@@ -40,15 +38,3 @@ def create_dataloaders(inputs: Tensor,
                             batch_size=batch_size,
                             **data_loader_kwargs)
     return dataloader
-
-
-def get_text_and_labels(dat: pd.DataFrame, 
-                        text_col: str = None,
-                        label_col: str = None) -> tuple[List, List]:
-    if not text_col:
-        text_col = MODEL_CONFIG.input_col_name
-    if not label_col:
-        label_col = MODEL_CONFIG.target_col_name
-    texts = dat.loc[:, text_col].tolist()
-    labels = dat.loc[:, label_col].tolist()
-    return texts, labels
