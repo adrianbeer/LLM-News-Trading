aca7ddb0e4c81a950ea25dd0913f15cce32f6911
Adrian Beer <adrianbeer@fastmail.com>
2024-01-24 16:08:24 +0100

    Fix author and parenthesis handling.

--- src/utils/dataframes.py
+++ src/utils/dataframes.py
@@ -4,7 +4,7 @@ import pandas as pd
 from tqdm import tqdm
 
 def parallelize_dataframe(df, func, n_cores=4):
-    n_splits = n_cores*10
+    n_splits = n_cores
     df_split = np.array_split(df, n_splits)
     pool = Pool(n_cores)
     df = pd.concat(tqdm(pool.imap(func, df_split), total=n_splits))
