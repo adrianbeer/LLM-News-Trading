d99a2df4720e39816002e1c5fe7cd19d5551b154
Adrian <adrianbeer@fastmail.com>
2024-02-11 15:24:48 +0100

    working on indicator_applicator

--- src/preprocessing/indicator_applicator.py
+++ src/preprocessing/indicator_applicator.py
@@ -16,8 +16,6 @@ from tqdm.auto import tqdm
 from src.config import config
 from src.utils.tickers import get_tickers
 
-tickers = get_tickers(config.data.iqfeed.daily.cleaned)
-
 
 def get_insample_conditional_volatilies(ts: pd.Series):
     model = (arch_model(100 * ts, 
@@ -33,10 +31,38 @@ def get_insample_conditional_volatilies(ts: pd.Series):
     return cond_vol
 
 
-def add_indicators(ticker):
+def write_indicators(ticker):
     daily_price_path = f"{config.data.iqfeed.daily.cleaned}/{ticker}_daily.parquet"
     prices = pd.read_parquet(path=daily_price_path)
+    prices = add_indicators(prices)
+    prices.to_parquet(path=daily_price_path)
+
+
+indicators = ["std_252", "dollar_volume", 'r_intra_(t-1)', 'cond_vola']
+
+def add_indicators(prices):
     
+    if prices.shape[0] == 1:
+        # Return empty DataFFrame
+        prices.loc[:, indicators] = np.nan
+        return prices
+    
+    # If there are large holes in the time series we apply add_indicators recursively to each connected block 
+    timedeltas = prices.index.diff()
+    timedelta_mask = timedeltas >= pd.Timedelta("14 days")
+
+    # If unadjosted close is close to 0 we also split the time series, i.e. if there is a period where
+    # the time series has appropriate values before and after a certain "penny stock" period
+    prices["unadj_open"] = prices["adj_open"] / prices["cum_split_ratio"]
+    zero_price_mask = prices["unadj_open"] < 0.5
+    
+    mask = timedelta_mask | zero_price_mask
+    
+    if mask.any():
+        groupers = mask.cumsum()
+        prices = prices.groupby(groupers, group_keys=False).apply(add_indicators)
+        return prices
+
     # TODO: train_test split... prevent forward looking bias!
     # Requires train test split before using this module...  
     prices["std_252"] = prices["adj_close"].pct_change(fill_method=None).rolling(252, min_periods=252).std()*(252**0.5)
@@ -48,9 +74,7 @@ def add_indicators(ticker):
     
     prices["dollar_volume"] = prices["adj_volume"] * (prices["adj_close"] + prices["adj_open"])/2
     prices["r_intra_(t-1)"] = (prices["adj_close"] / prices["adj_open"] - 1).shift(periods=1)
-    prices["unadj_open"] = prices["adj_open"] / prices["cum_split_ratio"]
     
-    prices.to_parquet(path=daily_price_path)
     return prices
 
 
@@ -60,14 +84,17 @@ if __name__ == '__main__':
     parser.add_argument('--ticker', default=False)
     args = parser.parse_args()
     if args.ticker:
-        prices = add_indicators(args.ticker)
+        prices = write_indicators(args.ticker)
         print(prices.head())
         exit()
 
+    tickers = get_tickers(config.data.iqfeed.daily.cleaned)
     with tqdm(total=len(tickers), desc="tickers (indicator_applicator)", leave=True, position=0) as pbar:
-        # let's give it some more threads:
         with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-            futures = {executor.submit(add_indicators, ticker): ticker for ticker in tickers}
+            futures = {executor.submit(write_indicators, ticker): ticker for ticker in tickers}
             for future in concurrent.futures.as_completed(futures):
+                # This line will propagate any exceptions
+                check_for_exceptions = future.result()
+                
                 ticker = futures[future]
                 pbar.update(1)
