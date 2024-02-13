Fix incorrect order of class weights

src/model/neural_network.py
@@ -33,16 +33,14 @@ class BERTClassifier(pl.LightningModule):
             for param in self.bert.parameters():
                 param.requires_grad = False
                 
-        self.dropout = nn.Dropout(0.2)
+        self.dropout = nn.Dropout(0)
+        
         self.ff_layer: nn.Module = nn.Sequential(
-            nn.Dropout(0.2),
-            nn.Linear(self.bert.config.hidden_size, 20),
+            # nn.Dropout(0.2),
+            nn.Linear(self.bert.config.hidden_size, 10),
             nn.LeakyReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(20, 20),
-            nn.LeakyReLU(),
-            nn.Dropout(0.2),
-            nn.Linear(20, num_classes)
+            # nn.Dropout(0.2),
+            nn.Linear(10, num_classes)
         )
         
     def forward(self, input_ids, masks):
@@ -69,10 +67,12 @@ class BERTClassifier(pl.LightningModule):
         self.train_accuracy(preds, y)
         self.train_f1_score(preds, y)
     
-        self.log_dict({'train_loss (weighted)': weighted_loss,
-                       'train_loss': unweighted_loss,
+        self.log_dict({'train_loss': unweighted_loss,
                        "train_f1_score": self.train_f1_score,
                        "train_accuracy": self.train_accuracy}, 
+                      on_step=True, on_epoch=True, prog_bar=False)
+        
+        self.log_dict({'train_loss (weighted)': weighted_loss,}, 
                       on_step=True, on_epoch=True, prog_bar=True)
         return weighted_loss
     
@@ -87,7 +87,8 @@ class BERTClassifier(pl.LightningModule):
     
         self.log_dict({'val_loss': loss,
                        "val_f1_score": self.val_f1_score,
-                       "val_accuracy": self.val_accuracy})
+                       "val_accuracy": self.val_accuracy}, 
+                      prog_bar=True)
         return loss
     
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
@@ -96,7 +97,11 @@ class BERTClassifier(pl.LightningModule):
 
 def initialize_final_layer_bias_with_class_weights(model, weights):
     for name, param in model.ff_layer.named_parameters():
-        if name == "7.bias":
+        final_layer_bias_name = list(dict(model.ff_layer.named_parameters()).keys())[-1]
+        assert "bias" in final_layer_bias_name
+        
+        if name == final_layer_bias_name:    
+            assert len(param) == 3
             param.data[0] = weights.loc[0]
             param.data[1] = weights.loc[1]
             param.data[2] = weights.loc[2]