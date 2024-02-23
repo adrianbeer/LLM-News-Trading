56d081c8f01b7bd2e05ddadac31c193e057a887a
adrianbeer <adrianbeer@fastmail.com>
2024-02-23 19:10:17 +0100

    Split GPU intensive and CPU intensive part of staleness calculation

--- src/preprocessing/staleness.py
+++ src/preprocessing/staleness.py
@@ -13,76 +13,81 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batchsize', help='batch size')
+parser.add_argument('--generate_cls_tokens', help='')
+parser.add_argument('--calculate_staleness', help='')
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     batch_size = int(args.batchsize)
     
-    # Use baseline bert model to avoid look-ahead bias 
-    model = BertModel.from_pretrained(MODEL_CONFIG.pretrained_network)
-    model.eval()
+    if args.generate_cls_tokens:
+        # Use baseline bert model to avoid look-ahead bias 
+        model = BertModel.from_pretrained(MODEL_CONFIG.pretrained_network)
+        model.eval()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
-    model.to(device)
-    
-    dataset = pd.read_parquet(config.data.news.cleaned)
-    input_ids = pd.read_parquet(config.data.news.input_ids)
-    masks = pd.read_parquet(config.data.news.masks)
-    
-    if not dataset.index.name: 
-        dataset.index.name = "index"
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(device)
+        model.to(device)
         
-    original_index_name = dataset.index.name
-    print(f"{original_index_name=}")
+        dataset = pd.read_parquet(config.data.news.cleaned, columns="stocks")
+        input_ids = pd.read_parquet(config.data.news.input_ids)
+        masks = pd.read_parquet(config.data.news.masks)
+        
+        if not dataset.index.name: 
+            dataset.index.name = "index"
+            
+        original_index_name = dataset.index.name
+        print(f"{original_index_name=}")
 
-    # Align DataFrames
-    input_ids = input_ids.loc[dataset.index]
-    masks = masks.loc[dataset.index]
+        # Align DataFrames
+        input_ids = input_ids.loc[dataset.index]
+        masks = masks.loc[dataset.index]
 
-    # Convert to Tensors
-    input_ids = torch.from_numpy(input_ids.to_numpy())
-    masks = input_ids = torch.from_numpy(masks.to_numpy())
+        # Convert to Tensors
+        input_ids = torch.from_numpy(input_ids.to_numpy())
+        masks = input_ids = torch.from_numpy(masks.to_numpy())
 
-    dataloader = create_dataloader(tensors=[input_ids, masks], 
-                                batch_size=batch_size, 
-                                data_loader_kwargs=dict(shuffle=False))
-    cls_tokens = predict_cls(model, dataloader, device)
-    cls_tokens = pd.Series(index=dataset.index, data=list(cls_tokens))
+        dataloader = create_dataloader(tensors=[input_ids, masks], 
+                                    batch_size=batch_size, 
+                                    data_loader_kwargs=dict(shuffle=False))
+        cls_tokens = predict_cls(model, dataloader, device)
+        cls_tokens = pd.Series(index=dataset.index, data=list(cls_tokens))
+        cls_tokens.to_pickle("data/news/cls_tokens.pkl")
     
-    cls_tokens.to_pickle("data/bzg/cls_tokens.pkl")
-    
-    dataset["cls_token"] = cls_tokens
-    dataset["staleness"] = 0.0
-    n_of_sametime_news = 0
+    if args.calculate_staleness:
+        dataset = pd.read_parquet(config.data.news.cleaned)
+        cls_tokens = pd.read_pickle("data/news/cls_tokens.pkl")
+        dataset["cls_token"] = cls_tokens
+        dataset["staleness"] = 0.0
+        n_of_sametime_news = 0
 
-    # To determine the freshness of news, I compare the similarity of each news article with all articles published in the previous three days.
-    for ticker in tqdm(set(dataset.stocks), desc="stocks"):
-        ticker_news = dataset[dataset.stocks == ticker].reset_index()
-        ticker_news = ticker_news.set_index("time").sort_index(ascending=True)
-        # Set staleness of first news message to 0 
-        ticker_news.at[ticker_news.index[0], "staleness"] = 0
+        # To determine the freshness of news, I compare the similarity of each news article with all articles published in the previous three days.
+        for ticker in tqdm(set(dataset.stocks), desc="stocks"):
+            ticker_news = dataset[dataset.stocks == ticker].reset_index()
+            ticker_news = ticker_news.set_index("time").sort_index(ascending=True)
+            # Set staleness of first news message to 0 
+            ticker_news.at[ticker_news.index[0], "staleness"] = 0
 
-        for time in  ticker_news.index:
-            previous_news = ticker_news.loc[(time-pd.DateOffset(days=3)):time, "cls_token"]
-            if len(previous_news) == 1:
-                ticker_news.at[time, "staleness"] = 0
-            else:
-                try:
-                    current_cls = previous_news.pop(time)
-                except IndexError as e:
-                    n_of_sametime_news += 1
-                    current_cls = previous_news.iloc[-1]
-                    previous_news = previous_news.iloc[:-1]
+            for time in  ticker_news.index:
+                previous_news = ticker_news.loc[(time-pd.DateOffset(days=3)):time, "cls_token"]
+                if len(previous_news) == 1:
+                    ticker_news.at[time, "staleness"] = 0
+                else:
+                    try:
+                        current_cls = previous_news.pop(time)
+                    except IndexError as e:
+                        n_of_sametime_news += 1
+                        current_cls = previous_news.iloc[-1]
+                        previous_news = previous_news.iloc[:-1]
+                        
+                    cosine_sims = previous_news.apply(lambda x: dot(current_cls, x) / (norm(current_cls)*norm(x)))
+                    ticker_news.at[time, "staleness"] = cosine_sims.max()
                     
-                cosine_sims = previous_news.apply(lambda x: dot(current_cls, x) / (norm(current_cls)*norm(x)))
-                ticker_news.at[time, "staleness"] = cosine_sims.max()
-                
-        ticker_news.set_index(original_index_name, inplace=True)
-        # Add entries to data set
-        dataset.loc[ticker_news.index, "staleness"] = ticker_news.loc[:, "staleness"]
-        
-    print(n_of_sametime_news)
-    dataset.to_parquet(config.data.news.cleaned)
+            ticker_news.set_index(original_index_name, inplace=True)
+            # Add entries to data set
+            dataset.loc[ticker_news.index, "staleness"] = ticker_news.loc[:, "staleness"]
+            
+        print(n_of_sametime_news)
+        dataset.to_parquet(config.data.news.cleaned)
 
